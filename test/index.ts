b54c867aac5fac4b307c9e168706abcfea086497
lmssee <lmssee@outlool.com>
2024-07-02 12:23:35 +0800

    优化代码逻辑

--- test/index.ts
+++ test/index.ts
@@ -6,40 +6,39 @@ import {
   isTTY,
   throttle,
   typeOf,
-  TypeOf,
 } from '../index';
 import assert from 'node:assert';
 import test, { mock } from 'node:test';
 
 /** 测试数据类型判断 */
-test.skip('type of', t => {
+test.skip('type of', () => {
   assert.equal(typeOf(''), 'string');
   assert.notEqual(typeof new String(), typeOf(new String()));
   assert.equal(typeOf(new String()), 'string');
 });
 
 /** 测试当前是否为终端环境 */
-test.skip('is  tyy', t => {
+test.skip('is  tyy', () => {
   assert.equal(isTTY(), true);
 });
 /** 测试获取随机浮点数 */
-test.skip('get random float', t => {
+test.skip('get random float', () => {
   const testNumber = 1;
   assert.equal(getRandomFloat(testNumber) < testNumber, true);
 });
 
 /** 测试获取随机整数 */
-test.skip('get random int', t => {
+test.skip('get random int', () => {
   const testNumber = 10;
   assert.equal(getRandomInt(testNumber) < testNumber, true);
 });
 
 /** 测试节流 */
-test('throttle', t => {
+test('throttle', () => {
   // const fn = mock.fn();
   mock.timers.enable({ apis: ['setTimeout'] });
-  let resultNumber: { num: number } = { num: 0 };
-  const testFunc = throttle((a: any) => a.num++, 5000);
+  const resultNumber: { num: number } = { num: 0 };
+  const testFunc = throttle((a: { num: number }) => a.num++, 5000);
   testFunc(resultNumber);
   mock.timers.tick(2500);
   testFunc(resultNumber);
